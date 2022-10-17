181c426434f7ec6be4a060e639e373ee4ccbd965
Ollie Cross <iio.olivercross@gmail.com>
2022-10-17 19:04:58 +0100

    Added helper functions for getting data. Added get ALL warehouses route

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -3,4 +3,10 @@ const fs = require("fs");
 const { v4: uuid } = require("uuid");
 const helpers = require("../utils/helpers");
 
+//get ALL warehouses
+router.get("/", (req, res) => {
+  const warehouses = JSON.parse(fs.readFileSync("./data/warehouses.json"));
+  res.status(200).json(warehouses);
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const { PORT } = require(".env");
 const app = express();
+const warehouseRoutes = require("./routes/warehouseRoutes");
+const inventoryRoutes = require("./routes/inventoryRoutes");
 
-require("dontenv").config();
+//.ENV
+require("dotenv").config();
+const { PORT } = process.env;
 
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
 app.use("/warehouses", warehouseRoutes);
-app.use("/invetory", invemtoryRoutes);
+app.use("/inventory", inventoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
