78ff5c0d1bc2d9d43851ebfcfb9a6adeaff05ad3
Ollie Cross <iio.olivercross@gmail.com>
2022-10-18 10:39:50 +0100

    saving changes only

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const fs = require("fs");
 const { v4: uuid } = require("uuid");
-const { readWarehouses } = require("../utils/helpers");
+const { readWarehouses, readInventories } = require("../utils/helpers");
 
 //get ALL warehouses
 router.get("/", (req, res) => {
@@ -23,6 +23,12 @@ router.get("/:warehouseId", (req, res) => {
   !currentWarehouse
     ? res.status(404).send(`Could not find warehouse with id ${warehouseId}`)
     : res.status(200).json(currentWarehouse);
+});
+
+//get individual warehouse INVENTORY
+router.get("/:warehouseId/inventory", (req, res) => {
+  const warehouses = readInventories();
+  const warehouseId = req.params.warehouseId;
 });
 
 module.exports = router;
